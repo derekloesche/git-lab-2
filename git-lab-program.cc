b13cbb1e16fb2852b98c0d1c753056d97e241d8d
Derek Loesche <dl915217@ohio.edu>
2020-01-21 19:15:32 -0500

    initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Derek Loesche
+ *        Date: 21 January 2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
